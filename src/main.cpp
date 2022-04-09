1b1b3c44ef94039def39d55aad122eb38b2eca2d
theVerySharpFlat <aidenramen29@gmail.com>
2022-04-08 20:13:55 -0500

    Project setup + incbin

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
 #include <ostream>
 
+extern "C" {
+#include <incbin.h>
+}
+
+/*
+ * Load the templates into the binary because I don't feel like loading them from
+ * the filesystem
+ */
+INCTXT(commandTemplateCppFile, "../templates/command/templateCommand.cpp");
+INCTXT(commandTemplateHFile,   "../templates/command/templateCommand.h");
+
 int main() {
-    std::cout << "hello world" << std::endl;
     return 0;
 }
